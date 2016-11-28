24f8f6a4a6ab354fa58dbae9a96ea2feee0778bb
GaryKitkat7 <alexplanasobany7@gmail.com>
2016-11-28 18:59:23 +0100

    Merge branch 'master' into gerard
    # Conflicts:
    #	CalendarView-master-3/.idea/misc.xml

---
+++ firebase_prueba_bbdd/PruebaFireBase/app/src/main/java/com/example/mmontonv/pruebafirebase/PruebaFireBaseActivity.java
@@ -0,0 +1,48 @@
+package com.example.mmontonv.pruebafirebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class PruebaFireBaseActivity extends AppCompatActivity {
+
+    TextView mConditionTextView;
+    Button mButtonSunny;
+    Button mButtonFoggy;
+
+    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
+    DatabaseReference mConditionRef = mRootRef.child("condition");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prueba_fire_base);
+
+        //get UI elements
+        mConditionTextView = (TextView) findViewById(R.id.tvCondition);
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        mConditionRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String text = dataSnapshot.getValue(String.class);
+                mConditionTextView.setText(text);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
